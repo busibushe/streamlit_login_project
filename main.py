7f1544ac7e139df580c514b12b22964da268c1ed
busibushe <busibushe@gmail.com>
2025-07-09 09:48:52 +0700

    dattim

--- main.py
+++ main.py
@@ -137,8 +137,8 @@ except ValueError as e:
 # --- UI Filter ---
 unique_branches = sorted(df['Branch'].unique())
 selected_branch = st.sidebar.selectbox("Pilih Cabang", unique_branches)
-min_date = df['Sales Date'].min()
-max_date = df['Sales Date'].max()
+min_date = pd.to_datetime(df['Sales Date'].min()).date()
+max_date = pd.to_datetime(df['Sales Date'].max()).date()
 date_range = st.sidebar.date_input("Pilih Rentang Tanggal", value=(min_date, max_date), min_value=min_date, max_value=max_date)
 
 if len(date_range) != 2: st.warning("Mohon pilih rentang tanggal yang valid."); st.stop()
